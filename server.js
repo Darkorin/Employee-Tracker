44aa6c7ce200b9a1813352c2cd7770f30ef798de
David Stewart <darkorin00@hotmail.com>
2020-06-24 20:29:49 -0400

    basic server setup, no functionalty

---
+++ server.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "employee_db"
+  });
+
+  // connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
+const start = function() {
+    
+}
