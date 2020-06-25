2275edbaca4ce953da3aede0f748db3b34fd94c6
David Stewart <darkorin00@hotmail.com>
2020-06-24 22:42:15 -0400

    Finished menu prompts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const table = require("console.table");
+const employees = require("employees");
+const roles = require("roles");
+const dept = require("dept");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -20,30 +22,30 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
+    console.log("Welcome to the Employee Management System!");
     start();
   });
 
 const start = function() {
-    console.log("Welcome to the Employee Management System!");
     inquirer
     .prompt({
       name: "menu1",
       type: "list",
       message: "Main Menu:",
-      choices: ["View", "Add", "Update", "Delete", "Exit"]
+      choices: ["Manage Employees", "Manage Roles", "Manage Departments", "View Budget", "Exit"]
     }).then(({menu1}, err)=> {
         switch (menu1) {
-            case "View":
-                viewPrompt();
+            case "Manage Employees":
+                empPrompt();
                 break;
-            case "Add":
-                addPrompt();
+            case "Manage Roles":
+                rolePrompt();
                 break;
-            case "Update":
-                updatePrompt();
+            case "Manage Departments":
+                deptPrompt();
                 break;
-            case "Delete":
-                deletePrompt();
+            case "View Budget":
+                budgetPrompt();
                 break;
             case "Exit":
                 exit();
@@ -52,4 +54,109 @@ const start = function() {
                 throw err;
         }
     })
+}
+
+const empPrompt = () => {
+    inquirer
+    .prompt({
+      name: "menu2",
+      type: "list",
+      message: "Employee Menu:",
+      choices: ["View Employees", "Add Employees", "Update Employees", "Remove Employees", "Back"]
+    }).then(({menu2}, err)=> {
+        switch (menu2) {
+            case "View Employees":
+                employees.view();
+                break;
+            case "Add Employees":
+                employees.add();
+                break;
+            case "Update Employees":
+                employees.update();
+                break;
+            case "Remove Employees":
+                employees.remove();
+                break;
+            case "Back":
+                start();
+                break;
+            default:
+                throw err;
+        }
+    })
+}
+
+const rolePrompt = () => {
+    inquirer
+    .prompt({
+      name: "menu2",
+      type: "list",
+      message: "Employee Menu:",
+      choices: ["View Roles", "Add Roles", "Update Roles", "Remove Roles", "Back"]
+    }).then(({menu2}, err)=> {
+        switch (menu2) {
+            case "View Roles":
+                roles.view();
+                break;
+            case "Add Roles":
+                roles.add();
+                break;
+            case "Update Roles":
+                roles.update();
+                break;
+            case "Remove Roles":
+                roles.remove();
+                break;
+            case "Back":
+                start();
+                break;
+            default:
+                throw err;
+        }
+    })
+}
+
+const deptPrompt = () => {
+    inquirer
+    .prompt({
+      name: "menu2",
+      type: "list",
+      message: "Employee Menu:",
+      choices: ["View Departments", "Add Departments", "Remove Departments", "Back"]
+    }).then(({menu2}, err)=> {
+        switch (menu2) {
+            case "View Departments":
+                dept.view();
+                break;
+            case "Add Departments":
+                dept.add();
+                break;
+            case "Remove Departments":
+                dept.remove();
+                break;
+            case "Back":
+                start();
+                break;
+            default:
+                throw err;
+        }
+    })
+}
+
+const budgetPrompt = () => {
+    inquirer
+    .prompt({
+      name: "menu2",
+      type: "list",
+      message: "Employee Menu:",
+      choices: ["Manage Employees", "Manage Roles", "Manage Departments", "View Budget", "Exit"]
+    }).then(({menu2}, err)=> {
+    
+    })
+}
+
+const exit = () => {
+    console.log("Exiting Employee Management System.");
+    connection.end();
+    process.exit();
 }
